e20b745e4e2ebc29cadf962e9dc6ebaa459f1732
Flavio H Sousa <beatyourcitation@outlook.com>
2019-06-28 10:59:31 -0300

    Nova classe forma de pagamento

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuickBuy.Dominio.ObjetoDeValor;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,10 +10,18 @@ namespace QuickBuy.Dominio.Entidades
         public int Id { get; set; }
         public DateTime DataPedido { get; set; }
         public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+        public string CEP { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroDoEndereco { get; set; }
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
 
         /// <summary>
-        /// Pedido deve ter pelo menos um pedido
-        /// ou muitos pedidos
+        /// Pedido deve ter pelo menos um item de pedido
+        /// ou muitos itens de pedidos
         /// </summary>
         public ICollection<ItemPedido> ItemPedidos { get; set; }
     }
