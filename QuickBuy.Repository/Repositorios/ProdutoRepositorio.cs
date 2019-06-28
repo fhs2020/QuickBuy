4a85ef29f9644fdc58062f89806746a2fb42e556
Flavio H Sousa <beatyourcitation@outlook.com>
2019-06-28 17:37:11 -0300

    Novas classes de repositorio

---
+++ QuickBuy.Repository/Repositorios/ProdutoRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using QuickBuy.Dominio.Contratos;
+using QuickBuy.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repository.Repositorios
+{
+    public class ProdutoRepositorio : BaseRepositorio<Produto>, IProdutoRespositorio
+    {
+        public ProdutoRepositorio()
+        {
+
+        }
+    }
+}
