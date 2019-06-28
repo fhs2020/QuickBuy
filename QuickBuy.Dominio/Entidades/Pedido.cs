44ba184342f7048e0301a561ecf3a380ec62247b
Flavio H Sousa <beatyourcitation@outlook.com>
2019-06-28 10:04:06 -0300

    Novas classes de entidade

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+
+        /// <summary>
+        /// Pedido deve ter pelo menos um pedido
+        /// ou muitos pedidos
+        /// </summary>
+        public ICollection<ItemPedido> ItemPedidos { get; set; }
+    }
+}
