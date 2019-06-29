74f5979a36e08c1c42b39cb2802d58abe61af5ef
Flavio H Sousa <beatyourcitation@outlook.com>
2019-06-29 17:53:20 -0300

    Metodo ModelBuilder e classes de configuracoes

---
+++ QuickBuy.Repository/Contexto/QuickBuyContext.cs
@@ -0,0 +1,38 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using QuickBuy.Dominio.Entidades;
+using QuickBuy.Dominio.ObjetoDeValor;
+using QuickBuy.Repository.Config;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repository.Contexto
+{
+    public class QuickBuyContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<ItemPedido> ItensPedidos { get; set; }
+        public DbSet<FormaPagamento> FormaPagamento { get; set; }
+
+        public QuickBuyContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //// Classes de mapeamento aqui...
+            modelBuilder.ApplyConfiguration(new UsuarioConfiguration());
+            modelBuilder.ApplyConfiguration(new ProdutoConfiguration());
+            modelBuilder.ApplyConfiguration(new PedidoConfiguration());
+            modelBuilder.ApplyConfiguration(new ItemPedidoConfiguration());
+            modelBuilder.ApplyConfiguration(new FormaPagamentoConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
