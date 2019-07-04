07dd6c2109c17423c537a3d135b3a25d8a712167
Flavio H Sousa <beatyourcitation@outlook.com>
2019-07-04 10:38:26 -0300

    Nova tela de login

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,1 +1,15 @@
-﻿
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
+
+})
+
+export class LoginComponent {
+
+
+
+
+}
